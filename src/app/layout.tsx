694bf19450d3ece41a7ac6d0aedd6daf1025e718
Aditya Tajanpure <aditya@thetechnologymaker.com>
2024-05-25 15:39:42 +0530

    fixed minify error react

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,17 +16,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
-      <script
-        type="text/javascript"
-        dangerouslySetInnerHTML={{
-          __html: `(function(c,l,a,r,i,t,y){
+      <body className={inter.className}>
+        {children}
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `(function(c,l,a,r,i,t,y){
         c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
         t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
         y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
         })(window, document, "clarity", "script", "mgg9ygmso7");`,
-        }}
-      />
+          }}
+        />
+      </body>
     </html>
   );
 }
